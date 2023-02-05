ecf502e41786b4bab96f5fc3b823cd360a0a64d0
“Your <echosmania@gmail.com>
2023-02-04 22:43:42 -0300

    Fix market card

--- app/components/MarketCard.tsx
+++ app/components/MarketCard.tsx
@@ -1,19 +1,15 @@
 import Image from "next/image"
 
 interface Props {
-  img: string,
   name: string
+  img: string,
 }
 
-const MarketCard = ({ img, name }: Props) => {
+const MarketCard = ({ name, img }: Props) => {
   return (
-    <div className="bg-white flex gap-3 p-3 rounded-md cursor-pointer hover:scale-105 transition-transform duration-150">
-      <div>
-        <Image className="h-12 w-12 rounded border-black border" src={img} width="500" height="500" alt={`${name}-logo`} />
-      </div>
-      <div className="flex items-center">
-        <h3>{name}</h3>
-      </div>
+    <div className="bg-white flex gap-3 p-3 rounded-lg cursor-pointer hover:scale-105 transition-transform duration-150">
+      <Image className="h-12 w-12 rounded border-black border" src={img} width="500" height="500" alt={`${name}-logo`} />
+      <p className="flex items-center">{name}</p>
     </div>
   )
 }
