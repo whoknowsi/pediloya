7b3feb12c8c724e9f0c18a2434e2d38ccc888757
“Your <echosmania@gmail.com>
2023-02-05 09:00:53 -0300

    Change tag

--- app/components/MarketCard.tsx
+++ app/components/MarketCard.tsx
@@ -11,7 +11,7 @@ const MarketCard = ({ name, img }: Props) => {
     <ItemContainer pointer hover flex>
       <div className="flex gap-3 max-w-lg min-w-fit">
         <Image className="h-12 w-12 rounded border-black border" src={img} width="500" height="500" alt={`${name}-logo`} />
-        <p className="flex items-center">{name}</p>
+        <span className="flex items-center">{name}</span>
       </div>
     </ItemContainer>
   )
