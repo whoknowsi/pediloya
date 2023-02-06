9e532063f43a802df42fa3657854f3a627f2681a
“Your <echosmania@gmail.com>
2023-02-06 00:33:02 -0300

    Adding add and remove to product

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
 import { useEffect, useRef, useState } from "react"
 import ItemContainer from "./ItemContainer"
-import { AiOutlinePlus } from 'react-icons/ai'
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
+import { HiOutlineTrash } from 'react-icons/hi2'
 
 interface Market {
   name: string,
@@ -48,6 +49,8 @@ const getPriceRange = (markets: Market[]) => {
 
 const ProductCard = ({ src, name, id, markets }: Props) => {
   const [selectedMarket, setSelectedMarket] = useState<string>('')
+  const [timeoutProds, setTimeoutProds] = useState<ReturnType<typeof setTimeout> | null>(null)
+  const [productCount, setProductCount] = useState(0)
   const priceRange = useRef(getPriceRange(markets))
 
   const handlePriceClick = (marketId: string) => {
@@ -59,10 +62,49 @@ const ProductCard = ({ src, name, id, markets }: Props) => {
     )
   }
 
+  const handleProductEdit = (product: number) => {
+    timeoutProds && clearTimeout(timeoutProds) 
+    setTimeoutProds(setTimeout(() => setTimeoutProds(null), 3000))
+    
+    setProductCount(productCount + product)
+  }
+
   return (
     <ItemContainer flex>
       <div className="flex flex-col gap-4 max-w-xs h-full relative">
-        {(selectedMarket || markets.length === 1) && <div className="absolute right-0 top-0 cursor-pointer border p-1 rounded border-gray-500"><AiOutlinePlus /></div>}
+        {(selectedMarket || markets.length === 1) 
+          && 
+          (timeoutProds 
+            ? <div className="absolute flex right-0 top-0 px-2 cursor-pointer border rounded bg-white border-gray-600">
+              {
+                productCount > 1 
+                  ? <button className="w-7 h-7 flex items-center justify-center" onClick={() => handleProductEdit(-1)}>
+                      <AiOutlineMinus />
+                    </button>
+                  : <button className="w-7 h-7 flex items-center justify-center" onClick={() => {
+                    setProductCount(0)
+                    clearTimeout(timeoutProds)
+                    setTimeoutProds(null)
+                  }}>
+                      <HiOutlineTrash />
+                    </button>
+              }
+              
+              <span className="w-7 h-7 flex items-center justify-center">
+                {productCount}
+              </span>
+              <button className="w-7 h-7 flex items-center justify-center" onClick={() => handleProductEdit(1)}>
+                <AiOutlinePlus />
+              </button>
+            </div>
+            : <button 
+                className={`absolute flex items-center justify-center right-0 top-0 cursor-pointer border w-7 h-7 rounded border-gray-500 ${productCount > 0 ? 'bg-black text-white border-none' : ''}`}
+                onClick={productCount === 0 ? () => handleProductEdit(1) : () => handleProductEdit(0)}
+              >
+                {productCount > 0 ? productCount : <AiOutlinePlus />}
+              </button> )
+          
+        }
         <Image className="self-center" src={src} width={100} height={100} alt={name} />
         <div className="flex flex-col gap-1">
           <div className="tracking-wide">
