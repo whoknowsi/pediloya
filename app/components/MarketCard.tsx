a4a34e1b0455a168e10f6b03300f73717a99e12a
“Your <echosmania@gmail.com>
2023-02-09 23:28:31 -0300

    Smaller text on market card

--- app/components/MarketCard.tsx
+++ app/components/MarketCard.tsx
@@ -11,7 +11,7 @@ const MarketCard = ({ name, img }: Props) => {
     <ItemContainer pointer hover flex>
       <div className="flex gap-3 items-center">
         <Image className="rounded border-black border" src={img} width="60" height="60" alt={`${name}-logo`} />
-        <span className="truncate text-lg">{name}</span>
+        <span className="truncate text-base">{name}</span>
       </div>
     </ItemContainer>
   )
