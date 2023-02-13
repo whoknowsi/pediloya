c8cf4c8b697f691a871e7bc363c6f8ba78f7d1c2
“Your <echosmania@gmail.com>
2023-02-13 12:55:31 -0300

    Added header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Poppins } from '@next/font/google'
+import Link from 'next/link'
+import Image from 'next/image'
 const poppins = Poppins({ weight: ['300', '400', '500', '600', '700'], subsets: ['latin'] })
 
 export default function RootLayout({
@@ -14,7 +16,15 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header className="p-4 bg-white shadow-lg">
+          <Link className="flex gap-1 items-center cursor-pointer" href="/">
+            <Image src="/logo.png" alt="logo" width="35" height="35"/>
+            <h1 className="text-2xl font-bold italic text-red-first">PediloYa</h1>
+          </Link>
+        </header>
+        {children}
+      </body>
     </html>
   )
 }
