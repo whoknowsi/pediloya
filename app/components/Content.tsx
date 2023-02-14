b453783f17a75a1d1883393796105136f7b370a4
“Your <echosmania@gmail.com>
2023-02-13 21:58:25 -0300

    Using imgs from api to preview

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -5,7 +5,7 @@ import MarketCard from "./MarketCard";
 import ProductCard from "./ProductCard";
 import SearchBar from "./SearchBar";
 
-export function Content() {
+export default function Content() {
   const handleSearch = (value: string) => {
     console.log(value)
   }
@@ -14,63 +14,63 @@ export function Content() {
     <div className="flex flex-col items-center p-4 gap-4 max-w-5xl">
       <SearchBar handleSearch={handleSearch} />
       <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 justify-center w-full">
-        <MarketCard img='https://images.deliveryhero.io/image/pedidosya/restaurants/carrefour-116-market-sta-fe-nuevo-jpg.png' name='Carrefour - Market Chilavert (203)' />
-        <MarketCard img='https://images.deliveryhero.io/image/pedidosya/restaurants/logo-dia-supermercado-01.png' name='Dia - Villa Ballester (204)' />
+        <MarketCard img='https://pedidosya-api.whoknows.workers.dev/static/markets/63ccfd52aa705832ee237632.webp' name='Carrefour - Market Chilavert (203)' />
+        <MarketCard img='https://pedidosya-api.whoknows.workers.dev/static/markets/63ccfa15aa705832ee236776.webp' name='Dia - Villa Ballester (204)' />
       </section>
       <section className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 flex-wrap justify-between gap-4 w-full">
-        <CategoryCard img='https://images.deliveryhero.io/image/pedidosya/menu-sections/new-verticals/default/aceites.jpg' name='Aceites' />
-        <CategoryCard img='https://images.deliveryhero.io/image/pedidosya/menu-sections/new-verticals/default/agua.jpg' name='Aguas' />
-        <CategoryCard img='https://images.deliveryhero.io/image/pedidosya/menu-sections/new-verticals/default/ensaladas.jpg' name='Alimentos saludables' />
+        <CategoryCard img='/placeholder-product.png' name='Aceites' />
+        <CategoryCard img='/placeholder-product.png' name='Aguas' />
+        <CategoryCard img='/placeholder-product.png' name='Alimentos saludables' />
       </section>
       <section className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 flex-wrap justify-between gap-4 w-full">
         <ProductCard
           name="Aceite En Aerosol Fritolim Girasol 120 G."
-          src="https://images.deliveryhero.io/image/pedidosya/products/8c07b9a7-d04b-4e77-8752-c95f06e07648.jpg"
+          src="https://pedidosya-api.whoknows.workers.dev/static/products/63ccfa15aa705832ee236778.webp"
           id='1'
           markets={[
             {
               id: 'market1',
               name: 'Carrefour - Market Chilavert (203)',
               price: 520.10,
-              img: 'https://images.deliveryhero.io/image/pedidosya/restaurants/carrefour-116-market-sta-fe-nuevo-jpg.png'
+              img: 'https://pedidosya-api.whoknows.workers.dev/static/markets/63ccfd52aa705832ee237632.webp'
             },
             {
               id: 'market2',
               name: 'Dia - Villa Ballester (204)',
               price: 530.2,
-              img: 'https://images.deliveryhero.io/image/pedidosya/restaurants/logo-dia-supermercado-01.png'
+              img: 'https://pedidosya-api.whoknows.workers.dev/static/markets/63ccfa15aa705832ee236776.webp'
             }
           ]}
         />
         <ProductCard
           name="Aceite Lira Girasol 900 mL"
-          src="https://images.deliveryhero.io/image/pedidosya/products/3618f835-bda5-4090-896d-16a3086ddeda.jpg"
+          src="https://pedidosya-api.whoknows.workers.dev/static/products/63ccfd52aa705832ee23763e.webp"
           id='2'
           markets={[
             {
               id: 'market2',
               name: 'Dia - Villa Ballester (204)',
               price: 530,
-              img: 'https://images.deliveryhero.io/image/pedidosya/restaurants/logo-dia-supermercado-01.png'
+              img: 'https://pedidosya-api.whoknows.workers.dev/static/markets/63ccfa15aa705832ee236776.webp'
             },
             {
               id: 'market1',
               name: 'Carrefour - Market Chilavert (203)',
               price: 504,
-              img: 'https://images.deliveryhero.io/image/pedidosya/restaurants/carrefour-116-market-sta-fe-nuevo-jpg.png'
+              img: 'https://pedidosya-api.whoknows.workers.dev/static/markets/63ccfd52aa705832ee237632.webp'
             }
           ]}
         />
         <ProductCard
           name="Aceite De Girasol Pureza 1.5 L"
-          src="https://images.deliveryhero.io/image/pedidosya/products/7792180137944.jpg"
+          src="https://pedidosya-api.whoknows.workers.dev/static/products/63ccfa1baa705832ee2367e4.webp"
           id='3'
           markets={[
             {
               id: 'market1',
               name: 'Carrefour - Market Chilavert (203)',
               price: 759,
-              img: 'https://images.deliveryhero.io/image/pedidosya/restaurants/carrefour-116-market-sta-fe-nuevo-jpg.png'
+              img: 'https://pedidosya-api.whoknows.workers.dev/static/markets/63ccfd52aa705832ee237632.webp'
             },
           ]}
         />
