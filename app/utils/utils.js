18d9615f4315f1caf23806424416972aac5a7d31
“Your <echosmania@gmail.com>
2023-02-16 04:18:35 -0300

    Add function to remove ticks

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,9 +1,14 @@
+const accent_map = {'á':'a', 'é':'e', 'è':'e', 'í':'i','ó':'o','ú':'u','Á':'a', 'É':'e', 'è':'e', 'Í':'i','Ó':'o','Ú':'u', "'": ''}
+
 export const sortArray = (array) => {
-  return [...array].sort((a, b) => {
-    const nameA = a.name.toUpperCase()
-    const nameB = b.name.toUpperCase()
-    if (nameA > nameB) return 1
-    if (nameA < nameB) return -1
-    return 0
-  })
+  return [...array].sort((a, b) => a.name.localeCompare(b.name))
+}
+
+export const removeTicksFrom = (string) => {
+  if (!string) return ''
+  let ret = ''
+  for (var i = 0; i < string.length; i++) {
+    ret += accent_map[string.charAt(i)] !== undefined ? accent_map[string.charAt(i)] : string.charAt(i)
+  }
+  return ret
 }
