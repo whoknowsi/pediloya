ac33a99d8d79a6f01ce2dd80ce0e677c3c6a7f30
whoknowsi <echosmania@gmail.com>
2023-02-24 04:30:04 -0300

    Making non static main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,22 @@
+'use client'
+
 import Content from "./components/Content"
 import CategoryAside from "./components/CategoryAside";
 import { getCategories } from "./services/category-services"
 import { getMarkets, Market } from "./services/market-services";
+import { useEffect, useState } from "react";
+import { Category } from "./services/product-services";
 
-export default async function Home() {  
-  const categories = await getCategories()
-  
-  let markets:Market[] = []
+export default function Home() {  
+  const [categories, setCategories] = useState<Category[]>([])
+  const [markets, setMarkets] = useState<Market[]>([])
 
-  try {
-    markets = await getMarkets()
-  } catch(error) {
-    console.log(error)
-  }
+  useEffect(() => {
+    (async () => {
+      setMarkets(await getMarkets())
+      setCategories(await getCategories())
+    })()
+  }, [])
 
   return (
     <>
